7d86d15f5f8e0b524213ff73556e2277b9665536
Ian Morrill <ianmorrill@icloud.com>
2020-07-09 17:04:09 -0400

    Add database load and save file

---
+++ database_load_and_save.py
@@ -0,0 +1,37 @@
+def save_database(metadata, fingerprint_database, path):
+	"""Saves metadata and fingerprint database to a file.
+
+	Saves `metadata` and `fingerprint_database` to file specified by `path`.
+
+	Parameters
+	----------
+	metadata : List[Tuple[str, str, str]]
+		List of song song metadata as tuples of the form (name, artist, year)
+
+	fingerprint_database : Dict[Tuple[int, int, int], Tuple[int, int]]
+		Song metadata dictionary as dictionary mapping from (f_i, f_j, delta_t)
+		to (song ID, absolute position)
+
+	path : Path
+		The path to save the database file to
+	"""
+	pass
+
+def load_database(path):
+	"""Load metadata and fingerprint database from a file.
+
+	Load `metadata` and `fingerprint_database` from file specified by `path`.
+
+	Parameters
+	----------
+	path : Path
+		The path to save the database file to
+
+	Returns
+	-------
+	database : Tuple[List[Tuple[str, str, str]], 
+		Dict[Tuple[int, int, int], Tuple[int, int]]]
+		Tuple containing song metadata and the fingerprint database as
+		described for `save_database`
+	"""
+	pass
