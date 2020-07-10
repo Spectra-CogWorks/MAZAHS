cca41d601341b5f0b194d2e628249a79291f84ab
Ian Morrill <ianmorrill@icloud.com>
2020-07-10 11:05:53 -0400

    Add option to initialize the database

--- __init__.py
+++ __init__.py
@@ -1,10 +1,12 @@
 import argparse
+from pathlib import Path
 
 # Set up parameters
 parser = argparse.ArgumentParser(description="Automatically recognize songs")
 parser.add_argument("--update", "-u", action="store_true", help="Update the song database")
 parser.add_argument("--list", "-l", action="store_true", help="View list of files")
-parser.add_argument("--add", "-a", help="Path to MP3 file to add")
+parser.add_argument("--init", "-i", action="store_true", help="Initialize the database")
+parser.add_argument("--add", "-a", type=Path, help="Path to MP3 file to add")
 parser.add_argument("--title", "-t", help="Song title to add")
 parser.add_argument("--artist", "-A", help="Song artist to add")
 parser.add_argument("--year", "-y", help="Song year to add")
@@ -17,6 +19,9 @@ if args.update:
 	pass
 elif args.list:
 	# List the database
+	pass
+elif args.init:
+	# Initialize the database
 	pass
 else:
 	# Recognize the song
