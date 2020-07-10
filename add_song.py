ad832c8c918cefffbd0b14864122b706f9734a2c
Rachna <rachgupta200@gmail.com>
2020-07-10 10:24:33 -0500

    Updated with iterator and new name for create_fingerprints

--- add_song.py
+++ add_song.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from samples_and_amp_array import get_mp3_samples, get_spec
-from fingerprint_creator import local_peaks, fingerprint_create
+from fingerprint_creator import local_peaks, fingerprints_create
 from database import save_database, load_database
 def add_song(song_name, artist, year, path = Path("file1.mp3")):
     """Adds new song to database and stores metadata in metadata list.
@@ -21,23 +21,24 @@ def add_song(song_name, artist, year, path = Path("file1.mp3")):
     None"""
     database_pathway = '' #insert path here
     #load database
-    (song_metadata, fingerprint_database) = load_database(database_pathway)
+    song_metadata, fingerprint_database = load_database(database_pathway)
     #append metadata to the list
     song_metadata.append((song_name,artist,year))
-    #call functions to get samples, create spectrogram, identify peaks, and create fingerprint
-    (fi, fj, delta_t, abs_time) = fingerprint_create(local_peaks(get_spec(get_mp3_samples(path), 44100)), 10)
-    #assign song_id
-    song_id = len(song_metadata) 
-    #add song data to fingerprint_database
-    key = (fi, fj, delta_t)
-    value = (song_id, abs_time)
-    if key in fingerprint_database:
-        fingerprint_database[key] = fingerprint_database[key].append(value)
-    else:
-        fingerprint_database[key] = [value]
-        #save database
+    #call functions to get samples, create spectrogram, identify peaks, and create fingerprints
+    song_fingerprints = fingerprints_create(local_peaks(get_spec(get_mp3_samples(path), 44100)), 10)
+    #assign song_id (first id is 0)
+    song_id = len(song_metadata) - 1
+    for fingerprint in song_fingerprints:
+        #unpack fingerprint and repack into key and value
+        fi, fj, delta_t, abs_time = fingerprint
+        key = (fi, fj, delta_t)
+        value = (song_id, abs_time)
+        #add fingerprint data to fingerprint_database
+        if key in fingerprint_database:
+            fingerprint_database[key] = fingerprint_database[key].append(value)
+        else:
+            fingerprint_database[key] = [value]
+    #save database
     save_database(song_metadata,fingerprint_database, database_pathway)
     #DONE
     return None
-    
-    
