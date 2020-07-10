dd8f7006c87b2dde39a44b20bdc763a58af4c1b6
Ian Morrill <ianmorrill@icloud.com>
2020-07-10 11:28:31 -0400

    Added ability to add songs and init the database

--- __init__.py
+++ __init__.py
@@ -1,11 +1,13 @@
 import argparse
 from pathlib import Path
+from add_song import add_song
+from database import init_database
 
 # Set up parameters
 parser = argparse.ArgumentParser(description="Automatically recognize songs")
 parser.add_argument("--update", "-u", action="store_true", help="Update the song database")
 parser.add_argument("--list", "-l", action="store_true", help="View list of files")
-parser.add_argument("--init", "-i", action="store_true", help="Initialize the database")
+parser.add_argument("--init", "-i", type=Path, help="Initialize the database")
 parser.add_argument("--add", "-a", type=Path, help="Path to MP3 file to add")
 parser.add_argument("--title", "-t", help="Song title to add")
 parser.add_argument("--artist", "-A", help="Song artist to add")
@@ -16,13 +18,13 @@ args = parser.parse_args()
 
 if args.update:
 	# Update the database
-	pass
+	add_song(args.title, args.artist, args.year, args.add)
 elif args.list:
 	# List the database
 	pass
-elif args.init:
+elif args.init != None:
 	# Initialize the database
-	pass
+	init_database(args.init)
 else:
 	# Recognize the song
 	pass
