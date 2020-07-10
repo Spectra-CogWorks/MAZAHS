f3d16897afc220e93729d31469bc709f1e60ed2e
mdas64 <missmilid@gmail.com>
2020-07-10 11:33:14 -0400

    fingerprint_creator.py

--- fingerprint_creator.py
+++ fingerprint_creator.py
@@ -2,11 +2,103 @@
 Module to find and analyze peaks in spectrogram data for sound samples and create fingerprints for samples using pattern fanouts
 """
 
-def local_peaks(spec_samples):
-	"""
-	"""
-	# ! Delete this pass
-	pass
+from numba import njit
+
+# `@njit` "decorates" the `_peaks` function. This tells Numba to
+# compile this function using the "low level virtual machine" (LLVM)
+# compiler. The resulting object is a Python function that, when called,
+# executes optimized machine code instead of the Python code
+
+@njit
+def _peaks(data_2d, rows, cols, amp_min):
+    """
+    A Numba-optimized 2-D peak-finding algorithm. This is just a helper function.
+    Use "local_peak_locations" instead if you want to get the peaks.
+    
+    Parameters
+    ----------
+    data_2d : numpy.ndarray, shape-(H, W)
+        The 2D array of data in which local peaks will be detected.
+
+    rows : numpy.ndarray, shape-(N,)
+        The 0-centered row indices of the local neighborhood mask
+    
+    cols : numpy.ndarray, shape-(N,)
+        The 0-centered column indices of the local neighborhood mask
+        
+    amp_min : float
+        All amplitudes at and below this value are excluded from being local 
+        peaks.
+    
+    Returns
+    -------
+    List[Tuple[int, int]]
+        (row, col) index pair for each local peak location. 
+    """
+    peaks = []
+    
+    # iterate over the 2-D data in col-major order
+    for c, r in np.ndindex(*data_2d.shape[::-1]):
+        if data_2d[r, c] <= amp_min:
+            continue
+
+        for dr, dc in zip(rows, cols):
+            # don't compare element (r, c) with itself
+            if dr == 0 and dc == 0:
+                continue
+
+            # mirror over array boundary
+            if not (0 <= r + dr < data_2d.shape[0]):
+                dr *= -1
+
+            # mirror over array boundary
+            if not (0 <= c + dc < data_2d.shape[1]):
+                dc *= -1
+
+            if data_2d[r, c] < data_2d[r + dr, c + dc]:
+                break
+        else:
+            peaks.append((r, c))
+    return peaks
+
+def local_peak_locations(data_2d, neighborhood, peak_threshold):
+    """
+    Defines a local neighborhood and finds the local peaks
+    in the spectrogram, which must be larger than the specified `peak_threshold`.
+    
+    Parameters
+    ----------
+    data_2d : numpy.ndarray, shape-(H, W)
+        The 2D array of data in which local peaks will be detected.
+        Comes from the spectrogram.
+    
+    neighborhood : numpy.ndarray, shape-(h, w)
+        A boolean mask indicating the "neighborhood" in which each
+        datum will be assessed to determine whether or not it is
+        a local peak. h and w must be odd-valued numbers
+        
+    amp_min : float
+        All amplitudes at and below this value are excluded from being local 
+        peaks.
+    
+    Returns
+    -------
+    List[Tuple[int, int]]
+        (row, col) index pair for each local peak location.
+    
+    Notes
+    -----
+    The local peaks are returned in column-major order.
+    """
+    rows, cols = np.where(neighborhood)
+    assert neighborhood.shape[0] % 2 == 1
+    assert neighborhood.shape[1] % 2 == 1
+
+    # center neighborhood indices around center of neighborhood
+    rows -= neighborhood.shape[0] // 2
+    cols -= neighborhood.shape[1] // 2
+    
+    return _peaks(data_2d, rows, cols, amp_min=peak_threshold)
 
 def fingerprint_create(peaks, fanoutVal):
 	"""
