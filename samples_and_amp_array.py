bdefa42fecc8218445fc98e1e56e4664df8f69a4
esthershen03 <64560472+esthershen03@users.noreply.github.com>
2020-07-10 11:21:54 -0400

    Update spec function, add docstrings and comments

--- samples_and_amp_array.py
+++ samples_and_amp_array.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
-from typing import Tuple
 import librosa
 from IPython.display import Audio
 from typing import Union, Callable, Tuple
@@ -9,6 +8,25 @@ from pathlib import Path
 
 # normalizing arrays
 def rescale(array, new_min, new_max):
+    """
+    Normalizes all the samples to a common range.
+    
+    Parameters
+    ----------
+    array : numpy.ndarray
+        A shape-(N,) array that needs to be rescaled.
+    
+    new_min : int
+        The desired minimum of the array after scaling.
+
+    new_max : int
+        The desired maximum of the array after scaling.
+
+    Returns
+    -------
+    numpy.ndarray
+        A shape-(N,) array rescaled to the desired range.
+    """
     minimum, maximum = np.min(array), np.max(array)
     ratio = (new_max - new_min) / (maximum - minimum)
     scaled_array = ratio * array
@@ -17,7 +35,20 @@ def rescale(array, new_min, new_max):
 
 # microphone samples
 def get_mic_samples(listen_time):
-
+    """
+    Uses microphone to record and digitize an analog signal.
+    
+    Parameters
+    ----------
+    listen_time : float
+        Length of recording in seconds.
+    
+    Returns
+    -------
+    samples : numpy.ndarray
+            A shape-(N,) array of samples extracted from the analog signal, scaled to the range (-2**15, 2**15).
+    """
+    # record audio through mic
     from microphone import record_audio
     frames, sample_rate = record_audio(listen_time)
 
@@ -28,12 +59,24 @@ def get_mic_samples(listen_time):
 
 # mp3 samples
 def get_mp3_samples(file_path):
-
+    """
+    Loads a local mp3 file and digitizes the analog signal.
+    
+    Parameters
+    ----------
+    file_path : Union[str, pathlib.Path]
+        Path to the mp3 file. E.g. "my_audio.npy" will load an audio
+        file called "my_audio.npy" to the current working directory.
+    
+    Returns
+    -------
+    samples : numpy.ndarray
+            A shape-(N,) array of samples extracted from the analog signal, scaled to the range (-2**15, 2**15).
+    """
     local_song_path = Path(file_path)
 
     # load the digital signal for the song
     samples, sampling_rate = librosa.load(local_song_path, sr=44100, mono=True)
-    samples = np.interp(samples, (samples.min(), samples.max()), (-2**15, 2**15))
 
     return rescale(samples, -2**15, 2**15)
 
@@ -41,6 +84,22 @@ def get_mp3_samples(file_path):
 sampling_rate = 44100 # or set to whatever
 
 def get_spec(samples, sampling_rate):
+    """
+    Returns the 2d-array of spectrogram data.
+    
+    Parameters
+    ----------
+    samples : numpy.ndarray
+        A shape-(N,) array of samples extracted from the analog signal.
+
+    sampling_rate : int
+        The sampling rate at which the samples were extracted.
+    
+    Returns
+    -------
+    S : numpy.ndarray
+            A 2d-array of Fourier coefficients organized by frequencies (rows) and times (columns).
+    """
     S, freqs, times = mlab.specgram(
         samples,
         NFFT=4096,
@@ -49,4 +108,7 @@ def get_spec(samples, sampling_rate):
         noverlap=int(4096 / 2),
         mode='magnitude'
     )
+
+    S[S == 0] = 1e-20
+
     return S
