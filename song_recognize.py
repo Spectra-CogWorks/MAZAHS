3f54154c53ed3d83b7a7a571eba1f7b961de0566
jekthewarrior <pranav16@outlook.com>
2020-07-10 13:30:35 -0700

    Redid offset time calculation in seconds

--- song_recognize.py
+++ song_recognize.py
@@ -25,12 +25,15 @@ def test_sample(record_time):
 	-------
 	
 	"""
+	song_metadata, database = db.load_database()
 	song_fingerprints = fingerprints_create(local_peak_locations(get_spec(get_mic_samples(record_time), 44100)))
 	song_id, offset, pastcutoff = determine_song(tally(song_fingerprints))
 	
-	# Calculate the offset time using the formula offset_time = recording time * (offset / total samples)
-	total_samples = 44100*record_time
-	offset_time = record_time * (offset/total_samples)
+	# Calculate the offset time using the formula offset_time = total song time * (offset / total song samples)
+	# total song samples = 44100 * total song time
+	a, b, c, time = song_metadata[song_id]
+	total_samples = 44100 * time
+	offset_time = time * (offset/total_samples)
 	
 	# Return the song ID and offset_time
 	return song_id, offset_time, pastcutoff
