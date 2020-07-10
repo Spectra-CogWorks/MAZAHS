89efcbbe73daa9fea602e7371be10fe8d3eff689
jekthewarrior <pranav16@outlook.com>
2020-07-09 21:38:20 -0700

    Fixed and finalized base functionality of fingerprint_create()

--- fingerprint_creator.py
+++ fingerprint_creator.py
@@ -5,10 +5,10 @@ Module to find and analyze peaks in spectrogram data for sound samples and creat
 def local_peaks(spec_samples):
 	"""
 	"""
+	# ! Delete this pass
+	pass
 
-	return 
-
-def fingerprint_creation(peaks, fanoutVal):
+def fingerprint_create(peaks, fanoutVal):
 	"""
 	Creates a fingerprint by creating a list of fanout patterns based on each peak traversed in order of 
 	ascending frequency and then time
@@ -29,10 +29,14 @@ def fingerprint_creation(peaks, fanoutVal):
 		and time interval between the two peaks
 
 	"""
-	# TODO Test the function with sample data and fix bugs
+	# // TODO Test the function with sample data and fix bugs
 	# TODO Optimize the code once the base design is working and has base functionality
+	
 	# Initialize the fingerprints array
 	fingerprint = []
+	
+	# Check that there are enough peaks for the fanout value otherwise return a more informative error
+	assert len(peaks) > fanoutVal, "There are too few peaks to create a fingerprint"
 
 	# Iterate over the peaks array which should be already organized by ascending frequency and time
 	for i in range(len(peaks)-fanoutVal):
@@ -40,7 +44,8 @@ def fingerprint_creation(peaks, fanoutVal):
 		for n in range(1, fanoutVal+1):
 			# The time_interval is calculated as the time of the fanout_peak minus the time of the initial_peak
 			# Append the tuple to the list (initial_peak, fanout_peak, initial_time, time_interval)
-			fingerprint.append(peaks[i], peaks[i+n], peaks[i][0], peaks[i+n][0]-peaks[i][0])
+			# The tuple contains these values peaks[i], peaks[i+n], peaks[i][0], peaks[i+n][0]-peaks[i][0]
+			fingerprint.append((peaks[i], peaks[i+n], peaks[i][0], peaks[i+n][0]-peaks[i][0]))
 	
 	# Return the fingerprints list
 	return fingerprint
